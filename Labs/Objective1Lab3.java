6ccfbeccbddc9b49339f732383fbf191a5d889ad
Ankitshah89 <shahdankit@gmail.com>
2020-01-01 21:44:47 -0500

    complete Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3 {
+  public static void main (String[] args){
+    System.out.println("Name : Ankit Shah ");
+    System.out.println("Age : 30 ");
+    System.out.println("State : Maine ");
+
+    
+  }
+}
