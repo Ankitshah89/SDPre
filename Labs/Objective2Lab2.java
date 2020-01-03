5bbaccb972c5eb42b279fc3754253af0cb0139e8
Ankitshah89 <shahdankit@gmail.com>
2020-01-02 20:34:50 -0500

    Complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ankit Shah";
+    int age = 30;
+    String state = "Maine";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+
+
+  }
+}
