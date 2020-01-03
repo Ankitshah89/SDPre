05c7261fed48031a5ee3760f439f2e04d6cae872
Ankitshah89 <shahdankit@gmail.com>
2020-01-02 20:45:35 -0500

    Complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = " Momo";
+    food2 = " Chowmein";
+    food3 = " Dumplings";
+
+    System.out.println("My Favourite foods are: \n1: " + food1 + "\n2: "  + food2 + "\n3: " + food3);
+
+
+
+  }
+}
