b572326b63fb18235df425c897396f4cb5ba1e2f
Ankitshah89 <shahdankit@gmail.com>
2020-01-07 11:03:55 -0500

    Complete Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Objective7Lab1{
+  public static void main(String[] args){
+    printname();
+
+    /*String name;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    name = input.nextLine();
+    String n = printname(name);
+    System.out.println("Hello " + n);
+  }
+  public static String printname(String name){
+    return name;*/
+    
+  }
+  public static void printname(){
+    System.out.println("Hello Ankit.");
+  }
+}
